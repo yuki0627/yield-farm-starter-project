08473495847bbea8072b8159a25c2d0249a95863
Yuki Takahashi <yuki0627@gmail.com>
2022-10-19 05:34:25 +0900

    トークン作成

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,21 @@
-const TokenFarm = artifacts.require("TokenFarm");
+const TokenFarm = artifacts.require(`TokenFarm`)
+const DappToken = artifacts.require(`DappToken`)
+const DaiToken = artifacts.require(`DaiToken`)
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenFarm);
-};
+module.exports = async function(deployer, newtwork, accounts) {
+
+    await deployer.deploy(DaiToken)
+    const daiToken = await DaiToken.deployed()
+
+    await deployer.deploy(DappToken)
+    const dappToken = await DappToken.deployed()
+
+    await deployer.deploy(TokenFarm, dappToken.address, daiToken.address)
+    const tokenFarm = await TokenFarm.deployed()
+
+    //100万Dappデプロイする
+    await dappToken.transfer(tokenFarm.address, '1000000000000000000000000')
+
+    //100Daiデプロイする
+    await daiToken.transfer(accounts[1], '100000000000000000000')
+}
